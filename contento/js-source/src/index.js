cb54bc6251fb22475fb41947813bf244a7263440
gffuma <fumagalli.gf@gmail.com>
2016-10-21 15:42:59 +0200

    React ContentEditor

--- contento/js-source/src/index.js
+++ contento/js-source/src/index.js
@@ -1,3 +1,22 @@
-module.exports = {
-  boot: () => console.log('I am from Webpack!'),
-}
+import React from 'react'
+import ReactDOM from 'react-dom'
+import JsHook from './jshook'
+import ContentEditor from './components/ContentEditor'
+
+JsHook.register('content-editor', (element) => {
+  const regions = JSON.parse(element.value || '{}')
+  element.style.visibility = 'hidden'
+
+  const reactContainer = document.createElement('div')
+  element.parentNode.insertBefore(reactContainer, element)
+
+  ReactDOM.render(
+    <ContentEditor
+      regions={regions}
+      saveRegions={(regions) => element.value = JSON.stringify(regions)}
+    />,
+    reactContainer
+  )
+})
+
+module.exports = JsHook
